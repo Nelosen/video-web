034ab110d258b63bfea3d00d868a62ac01680f79
zl <zhoulin@xinyunlian.com>
2018-08-27 20:12:31 +0800

    home

--- src/app/page/index/module/home/index.tsx
+++ src/app/page/index/module/home/index.tsx
@@ -5,26 +5,14 @@ import * as style from "./style.pcss";
 import {Footer} from 'component/footer'
 import {store} from "./store";
 
-
 @observer
 export default class Home extends Component<any, any> {
-
-    public detailClick = () => {
-        this.props.history.push(`/business`);
-    };
-
-    public moreClick = () => {
-        this.props.history.push(`/business`);
-    };
 
     public render() {
         return (
             <div>
-                {/*<Ho/>*/}
                 <Banner/>
-                <Testimonial/>
-                <Button onClick={this.detailClick}>查看详情</Button>
-                <Button onClick={this.moreClick}>更多</Button>
+                <Testimonial {...this.props}/>
                 <Footer/>
             </div>
         )
@@ -39,7 +27,7 @@ export class Banner extends Component<any, any> {
 
             <div style={{width: '100%'}}>
                 <Carousel className={style.banner} autoplay={false}>
-                    <div >
+                    <div>
                         <img src={require('./images/two.jpg')}/>
                     </div>
                     <div>
@@ -60,35 +48,48 @@ export class Testimonial extends Component<any, any> {
         super();
         store.load()
     }
+
     // 立即购买
     public onClick = (id) => {
         return () => {
-            // this.props.history.push(`/business/detail/${id}`);
             const redirectUrl = process.env.REDIRECT_URL;
             window.location.href = redirectUrl + "pay.html#/?id=" + id;
         }
 
-    }
+    };
+
+    public detailClick = (id) => {
+        return() =>{
+            this.props.history.push(`/class/detail/${id}`);
+        }
+    };
+
+    public moreClick = () => {
+        this.props.history.push(`/class`);
+    };
+
     public render() {
         return (
             <div className={style.commonmid}>
                 <div className={style.classTop}>
-                    <p className={style.clThetitP} >
+                    <p className={style.clThetitP}>
                         <span className={style.clThetit}>精品课程</span>
                     </p>
                 </div>
-                <div className={style.all}>
-                    {
-                        store.list.map((item) => (
-                            <div  className={style.homeBoxItem}>
-                                <div className={style.homeHeading}>
-                                    <img src={item.img}/>
-                                </div>
-                                <h1>{item.title}</h1>
+                {
+                    store.list.map((item) => (
+                        <div className={style.homeBoxItem}>
+                            <div className={style.homeHeading}>
+                                <img src={item.img}/>
                             </div>
-                        ))
-                    }
-                </div>
+                            <h1>{item.title}</h1>
+                            <p>骑牛摆渡
+                                <a style={{color:'red',marginLeft:10}} onClick={this.detailClick(item.item_id)}>查看详情</a>
+                                <Button style={{background:'red',marginLeft:10,color:'#fff'}}  onClick={this.moreClick}>更多课程</Button>
+                            </p>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
