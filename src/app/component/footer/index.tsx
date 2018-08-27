a318da8d26447971a5a0f43df4c8db88107494f3
WIN-N9N5LPNC6UL\xyl <wuhuaidong@xinyunlian.com>
2018-08-27 19:24:48 +0800

    w

--- src/app/component/footer/index.tsx
+++ src/app/component/footer/index.tsx
@@ -1,26 +1,35 @@
 import * as React from 'react';
 import {observer} from 'mobx-react';
-import * as styles from './style.pcss';
+import * as style from  './style.pcss'
 
 @observer
 export class Footer extends React.Component<any, any> {
     public render() {
 
         return (
-            <div className={styles.footer}>
-                <div className={styles.footerDiv}>
-                    <div className={styles.footerDivLeft}>
-                        <h3>关于广锦</h3>
-                        <p>广锦商务咨询成立于2018年4月，致力于技术咨询，商务信息咨询，企业管理咨询”</p>
-                    </div>
-                    <div className={styles.footerDivRight}>
-                        <h3>联系我们</h3>
-                        <p>您可以通过以下途径和我们取得联系</p>
-                        <p>15579753601</p>
-                        <p>gjswr123@sohu.com</p>
+            <div className={style.kcommWid}>
+                <div className={style.footernUpart}>
+                    <div className={style.commWid}>
+                        <div className={style.nftblock}>
+                            <ul>
+                                <li className={style.abpTit}>关于我们</li>
+                                <li className={style.awra}>
+                                    <li className={style.awra}><a href="">用户注册协议</a></li>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={style.nsplitLine}></div>
+                        <div className={style.nftb}>
+                            <ul>
+                                <li className={style.abpTit}>联系我们</li>
+                                <li className={style.awra}>客服热线</li>
+                                <li className={style.aline}>18148585548</li>
+                                <li className={style.awra}>在线咨询</li>
+                                <li className={style.awra}>QQ:2640074370(09:00-17:30)</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-
             </div>
         )
     }
